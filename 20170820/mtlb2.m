145a7373ab99a8bf52c6b15872aa78d8ce6f26c8
Anchoret13 <vergil1133@live.com>
2017-08-21 11:30:58 +0800

    啊啊啊啊啊啊啊啊啊

--- 20170820/mtlb2.m
+++ 20170820/mtlb2.m
@@ -5,19 +5,19 @@ t=0:pi/100:2*pi;
 %z=0.6*sin(t);
 %x=2.5;
 %画圆柱
-y=0.89*cos(t);z=0.6*sin(t);x1=linspace(0,2.45,length(z));
-x=[meshgrid(x1)]';
-y=meshgrid(y);
-z=meshgrid(z);
-surf(x,y,z)
-axis equal
-for k=1:4
-view(-37.5,10*k)
-pause
-end
-[y,z]=meshgrid(y,z);
-x=0:0.1:2.45;
-surf(x,y,z);
+%y=0.89*cos(t);z=0.6*sin(t);x1=linspace(0,2.45,length(z));
+%x=[meshgrid(x1)]';
+%y=meshgrid(y);
+%z=meshgrid(z);
+%surf(x,y,z)
+%axis equal
+%for k=1:4
+%view(-37.5,10*k)
+%pause
+%end
+%[y,z]=meshgrid(y,z);
+%x=0:0.1:2.45;
+%surf(x,y,z);
 
 %画平面
 %syms h a
@@ -26,4 +26,12 @@ surf(x,y,z);
 %axis squre
 
 %喵的平面不画了直接作为约束条件好了（生气）
+syms
+x=-1.25+rand(1000,1)*2.5;
+y=-0.89+rand(1000,1)*1.78;
+z=-0.6+rand(1000,1)*1.2;
 
+InBuck=find(y.^2/(0.89)^2+z.^2/(0.6)^2<1);
+Oil=find(y.^2/(0.89)^2+z.^2/(0.6)^2<1&z<-x*tan(4.1)+0.4*tan(4.1)+1-0.6);
+T=1/InBuck;
+disp(T);

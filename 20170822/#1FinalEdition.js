29d1b120e6606b591f32af9b7384412e6087d125
Mr.StarGazer <vergil1133@live.com>
2017-08-24 01:46:33 +0800

    Update #1FinalEdition.js

--- 20170822/#1FinalEdition.js
+++ 20170822/#1FinalEdition.js
@@ -4,11 +4,11 @@ function Orders(){
     this.numC = Math.floor(Math.random()*100+1);
     this.numD = Math.floor(Math.random()*100+1);
     this.numE = Math.floor(Math.random()*100+1);
-    this.ta=this.numA/24;
-    this.tb=this.numB/24;
-    this.tc=this.numC/24;
-    this.td=this.numD/24;
-    this.te=this.numE/24;
+    this.ta=0;
+    this.tb=0;
+    this.tc=0;
+    this.td=0;
+    this.te=0;
     this.sum = this.numA+this.numB+this.numC+this.numD+this.numE;
     this.T=(this.numA+this.numB+this.numC+this.numD+this.numE)
 
@@ -19,49 +19,35 @@ function CreateOrderlists(){
     var OrderLists = new Array();
     var TA=0;
     var tSTO=new Array();
-    var tSigl=8;
     var EffiSTO=new Array();
     var EffiSigl=new Array();
-    var TotalTp1=0,TotalTp2=0;
-    var Quantum=0;
-    var Effi;
+    var EFFTSTO=0;
+    var EFFTSIGL=0;
     OrderLists[0] = new Orders();
+    OrderLists[0].ta=OrderLists[0].numA/24;
+    OrderLists[0].tb=OrderLists[0].numB/24;
+    OrderLists[0].tc=OrderLists[0].numC/24;
+    OrderLists[0].td=OrderLists[0].numD/24;
+    OrderLists[0].te=OrderLists[0].numE/24;
 
     for(let j=1;j<orderNum;j++){
         OrderLists[j] = new Orders();
         OrderLists[j].ta=Math.max(OrderLists[j].ta,OrderLists[j-1].tb);
         OrderLists[j].tb=Math.max(OrderLists[j].tb,OrderLists[j-1].tc-OrderLists[j].ta+OrderLists[j-1].tb);
         OrderLists[j].tc=Math.max(OrderLists[j].tc,OrderLists[j-1].td-OrderLists[j].ta-OrderLists[j].tb+OrderLists[j-1].tb);
-        OrderLists[j].td=Math.max(OrderLists[j].td,OrderLists[j-1].tb=OrderLists[j-1].tc+OrderLists[j-1].td-OrderLists[j].ta-OrderLists[j].tb-OrderLists[j].tc);
+        OrderLists[j].td=Math.max(OrderLists[j].td,OrderLists[j-1].tb-OrderLists[j-1].tc+OrderLists[j-1].td-OrderLists[j].ta-OrderLists[j].tb-OrderLists[j].tc);
         OrderLists[j].te=OrderLists[j].te;
         TA+=OrderLists[j].ta;
         tSTO[j]=TA+OrderLists[j].tb+OrderLists[j].tc+OrderLists[j].td+OrderLists[j].te;
         EffiSTO[j]=(OrderLists[j].sum)/tSTO[j];//仓储模式效率
+        EFFTSTO+=EffiSTO[j];
         EffiSigl[j]=(OrderLists[j].sum)/8;//单人模式效率
-        console.log(`完成第${j}笔订单使用仓储的效率为${EffiSTO[j]},而使用单人模式的效率为${EffiSigl[j]}`);
+        EFFTSIGL+=OrderLists[j].sum;
+        console.log(`${EffiSTO[j]} ${EffiSigl[j]}`);
     }
-    OrderLists[1] = new Orders();
-    OrderLists[1].ta=Math.max(OrderLists[1].ta,OrderLists[1-1].tb);
-    OrderLists[1].tb=Math.max(OrderLists[1].tb,OrderLists[1-1].tc-OrderLists[1].ta+OrderLists[1-1].tb);
-    OrderLists[1].tc=Math.max(OrderLists[1].tc,OrderLists[1-1].td-OrderLists[1].ta-OrderLists[1].tb+OrderLists[1-1].tb);
-    OrderLists[1].td=Math.max(OrderLists[1].td,OrderLists[1-1].tb=OrderLists[1-1].tc+OrderLists[1-1].td-OrderLists[1].ta-OrderLists[1].tb-OrderLists[1].tc);
-    OrderLists[1].te=Orderlists[1].te;
-    TotalTp1+=OrderLists[0].ta+OrderLists[0].tb+OrderLists[0].tc+OrderLists[0].td;
-    var TotalTp3=OrderLists[0].te+OrderLists[1].te;
-    for(let j=2;j<orderNum;j++){
-        OrderLists[j] = new Orders();
-        OrderLists[j].ta=Math.max(OrderLists[j].ta,OrderLists[j-1].tb);
-        OrderLists[j].tb=Math.max(OrderLists[j].tb,OrderLists[j-1].tb+OrderLists[j-1].tc-OrderLists[j].ta);
-        OrderLists[j].tc=Math.max(OrderLists[j].tc,OrderLists[j-1].tc+OrderLists[j-1].td-OrderLists[j].ta-OrderLists[j].tb,OrderLists[j-1].tb+OrderLists[j-1].tc+OrderLists[j-1].td-OrderLists[j].ta-OrderLists[j].tb);
-        OrderLists[j].td=Math.max(OrderLists[j].td,OrderLists[j-1].tb+OrderLists[j-1].tc+OrderLists[j-1].td-OrderLists[j].ta-OrderLists[j].tb-OrderLists[j].tc,OrderLists[j-1].tb+OrderLists[j-1].tc+OrderLists[j-1].td-OrderLists[j].ta-OrderLists[j].tb-OrderLists[j].tc,OrderLists[j-1].tb+OrderLists[j-1].tc+OrderLists[j-1].td-OrderLists[j].ta-OrderLists[j].tb-OrderLists[j].tc);
-        OrderLists[j].te=OrderLists[j].te;
-        TotalTp2+=(OrderLists[j].ta+OrderLists[j].tb+OrderLists[j].tc+OrderLists[j].td)-(OrderLists[j-1].ta+OrderLists[j-1].tb+OrderLists[j-1].tc+OrderLists[j-1].td);
-        TotalTp3+=OrderLists[j].te;
-        Quantum+=OrderLists[j].sum;
-    }
-    var TotalT = TotalTp1+TotalTp2+TotalTp3;
-    Effi=Quantum/TotalT;
-    console.log(`优化后的效率为${Effi}`);
+    var EFFAVGSTO=EFFTSTO/orderNum;
+    var EFFAVGSigl=EFFTSIGL/(8*orderNum);
+    var bili=EFFAVGSigl/EFFAVGSTO;
+    console.log(`相同随机数据生成的订单，若使用分区的平均效率为${EFFAVGSTO}，而使用单人模式的平均效率为${EFFAVGSigl}，单人模式的效率为分区效率的${bili*100}%`);
 }
-
-//CreateOrderlists();
+CreateOrderlists();
